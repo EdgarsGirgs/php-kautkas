7a0feb74ef75ccc2d1c1bb6ebd1d245d4258078b
unknown <you@example.com>
2024-09-20 08:06:43 +0300

    majas darbs

---
+++ Uzdevums/broken-code.php
@@ -0,0 +1,13 @@
+$name = "John;
+$age = 25;
+
+if ($age > 18) {
+    echo "Welcome, $name!"
+} else {
+    echo "Sorry, you are too young.";
+}
+
+$fruits = array("Abols", "Banans", "ķirši";
+echo "Mans mīļākais auglis ir " . $fruit[1];
+
+?>
